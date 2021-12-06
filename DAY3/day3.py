fcc081c47c942edba15a8d17c0841d959b594d1b
Richard Yau <ryau@vmware.com>
2021-12-06 13:51:41 -0500

    completed part 2

--- DAY3/day3.py
+++ DAY3/day3.py
@@ -39,3 +39,61 @@ gamma_bin   = ''.join([str(n) for n in total_g])
 epsilon_bin = ''.join([str(n) for n in total_e])
 
 print (int(gamma_bin, 2)*int(epsilon_bin,2))
+
+# Part II
+
+data = open('input.txt', 'r')
+list = data.readlines()
+
+# Remove items from list, 
+# input: list, index, binary bit 
+
+def removeItems(list, index, binary):
+    result = [x for x in list if int(x[index]) == binary]
+    list = result
+    return list
+
+
+for x in range(12):
+    one_count = 0
+    zero_count = 0
+    for line in list:
+        if int(line[x]) == 1:
+            one_count += 1
+        if int(line[x]) == 0:
+            zero_count += 1
+    if one_count < zero_count:
+        list = removeItems (list,x,0)
+    if one_count > zero_count or one_count == zero_count:
+        list = removeItems (list,x,1)
+    if len(list) < 1:
+        break
+
+oxy_bin   = list
+oxy_bin = ''.join([str(n) for n in list])
+
+data = open('input.txt', 'r')
+list = data.readlines()
+
+for x in range(12):
+    one_count = 0
+    zero_count = 0
+    for line in list:
+        if int(line[x]) == 1:
+            one_count += 1
+        if int(line[x]) == 0:
+            zero_count += 1
+    if one_count > zero_count:
+        list = removeItems (list,x,0)
+    if one_count < zero_count:
+        list = removeItems (list,x,1)
+    if one_count == zero_count:
+        list = removeItems (list,x,0)
+    if len(list) < 2:
+        break
+
+
+co2_bin = list
+co2_bin = ''.join([str(n) for n in list])
+
+print(int(co2_bin,2) * int(oxy_bin,2))
